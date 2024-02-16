b9c7f552d65a82884892e2afce84331b35ff5a6d
Bryuhn <bryuhn@gmail.com>
2024-02-16 12:40:30 +0100

    push

--- src/Components/profile.jsx
+++ src/Components/profile.jsx
@@ -5,10 +5,14 @@ import chart from '../assets/chart.png';
 const profile = () => {
   return (
     <div className="relative z-30 mt-10 w-full ">
-      <div className="mx-36 mb-32 mt-10 grid grid-cols-3 items-center justify-center gap-4 rounded-xl p-3 shadow-lg shadow-black outline">
+      <div
+        className="mx-36 mb-32 mt-10 grid grid-cols-3 items-center justify-center gap-4
+      rounded-xl p-3 shadow-lg shadow-black outline"
+      >
         <div
           name="card 1"
-          className="h-full w-full rounded-md bg-second p-5 shadow-md shadow-white outline outline-1 duration-150 ease-in-out hover:scale-[102%]"
+          className="h-full w-full rounded-md bg-second p-5 shadow-md shadow-white
+          outline outline-1 duration-150 ease-in-out hover:scale-[102%]"
         >
           <div className="col-span-2 flex justify-center py-2 text-xl font-bold text-white">
             <a>Position:</a>
@@ -68,7 +72,8 @@ const profile = () => {
         </div>
         <div
           name="card 2"
-          className="h-full w-full rounded-md bg-second p-5 shadow-md shadow-white outline outline-1 duration-150 ease-in-out hover:scale-[102%]"
+          className="h-full w-full rounded-md bg-second p-5
+          shadow-md shadow-white outline outline-1 duration-150 ease-in-out hover:scale-[102%]"
         >
           <div name="card header" className="grid grid-cols-2 outline-purple-600">
             <div className="col-span-2 flex justify-center py-2 text-xl font-bold text-white">
@@ -101,7 +106,7 @@ const profile = () => {
               <p className="px-2text-white">
                 Removal of boulders within a designated area in close proximately to the foundations of the wind
                 turbines so that the installation of the power lines allong a planed route could be fited without the
-                risk of delays due to damage to cable laying machines
+                risk of delays due to damage to cable laying follow up ships.
               </p>
             </div>
           </div>
@@ -112,9 +117,15 @@ const profile = () => {
             <div className="text-white">
               <br></br>
               <p className="px-2text-white">
-                Removal of boulders within a designated area in close proximately to the foundations of the wind
-                turbines so that the installation of the power lines allong a planed route could be fited without the
-                risk of delays due to damage to cable laying machines
+                Due to the close proximity of the wind turbines bases, it was of primery concern that vessel positing
+                and movement was of the utmost importance to avoid collisons with the sturcture. Acting as the online
+                surveyor, I directed the movement of both the ship and the ROV via walkie talkies. Remote mirrored qinsy
+                screens where setup in the bridge and the ROV command. Heading and distance was provided to bridge,
+                under way projected target postion was given to ROV pilot. Upon target locatoin verification, photo
+                imargy was taken of target on approach. Once ROV was in position, rov position was logged, bucket camera
+                captured overhead view. Bucket was relased and contents where verifed prior to vessel movement to drop
+                off zone. Grab scar was taken from ROV cameras and logged. Traget drop off location was taken, imagry of
+                drop off site was captured.
               </p>
             </div>
           </div>
@@ -123,11 +134,10 @@ const profile = () => {
               <a>Software</a>
             </div>
             <div className="text-white">
-              <br></br>
               <p className="px-2text-white">
-                Removal of boulders within a designated area in close proximately to the foundations of the wind
-                turbines so that the installation of the power lines allong a planed route could be fited without the
-                risk of delays due to damage to cable laying machines
+                Qinsy was used as the primary positioning software, linked up to the Bridge, ROV command and to the
+                onboard client representive quartiers. DVR linked to the ROV was used for visual target location and
+                capturing of photo imagery.
               </p>
             </div>
           </div>
@@ -138,16 +148,16 @@ const profile = () => {
             <div className="text-white">
               <br></br>
               <p className="px-2text-white">
-                Removal of boulders within a designated area in close proximately to the foundations of the wind
-                turbines so that the installation of the power lines allong a planed route could be fited without the
-                risk of delays due to damage to cable laying machines
+                A total of 1572 target boulders where located and moved outside of the the cable route and buffer zone.
+                An additional 104 targets where discoved and approved for removal by the onboard client representive.
               </p>
             </div>
           </div>
         </div>
         <div
           name="card 3"
-          className="h-full w-full rounded-md bg-second p-5 shadow-md shadow-white outline outline-1 duration-150 ease-in-out hover:scale-[102%]"
+          className="h-full w-full rounded-md bg-second p-5 shadow-md shadow-white outline
+           outline-1 duration-150 ease-in-out hover:scale-[102%]"
         >
           <div className="text-font-bold col-span-2 flex justify-center py-2 text-lg font-bold">
             <a>Vessel:</a>
